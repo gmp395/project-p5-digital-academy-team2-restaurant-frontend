60c25391032bdd8a75cb110a14bdff1d9d705be0
Gema <gmp395@gmail.com>
2026-09-10 22:04:09 +0200

    fix: adding home to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/Home.vue'
 import LoginView from '../Views/LoginView.vue'
 import RegisterView from '../Views/RegisterView.vue'
 
 const routes = [
-  { path: '/', name: 'login', component: LoginView, alias: '/login' },
+  { path: '/', name: 'home', component: Home, alias: '/home' },
+  { path: '/login', name: 'login', component: LoginView, alias: '/login' },
   { path: '/register', name: 'register', component: RegisterView },
 ]
 
